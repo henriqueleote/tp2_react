5a0d0085e0bf8ae8f52717eca0652539dbd457b3
Amatias13 <80266371+Amatias13@users.noreply.github.com>
2022-06-13 23:01:49 +0100

    Missing working

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, Image,TextInput } from 'react-native';
-import { Component } from 'react/cjs/react.production.min';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, View, Image, Text, StyleSheet, Dimensions } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
-import { ScrollView } from 'react-native-gesture-handler';
 
-const Missing = () => {
+const rows = 3;
+const cols = 2;
+const marginHorizontal = 7;
+const marginVertical = 7;
+const width = (Dimensions.get('window').width / cols) - (marginHorizontal * (cols + 1));
+const height = 200;
+
+const stylesGrid = StyleSheet.create({
+    scrollContainer: {
+      flex: 1,
+    },
+    sectionContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    boxContainer: {
+      marginTop: marginVertical,
+      marginBottom: marginVertical,
+      marginLeft: marginHorizontal,
+      marginRight: marginHorizontal,
+      width: width,
+      height: height,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#FFFFFF',
+    },
+  });
+
+const styles = StyleSheet.create({
+    body: {
+        position: 'absolute',
+        top:0,
+        height: '90%',
+        width: '100%',
+    },
+
+    missingBigText: {
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        marginVertical: 10,
+        fontSize: 24,
+        color: 'black',    
+        
+    },
+
+    missingName: {
+        position: 'absolute',
+        bottom: 0,
+        fontWeight: '500',
+        fontSize: 17, 
+        color: '#333333',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+    },
+
+    border: {
+        borderColor: '#000',
+        borderRadius: 10,
+        marginBottom: 20,
+        shadowColor: 'black',
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,  
+    },
+
+    borderTop: {
+        borderColor: '#000',
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        marginBottom: 20,
+        shadowColor: 'black',
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.5,
+        shadowRadius: 5,  
+    },
+
+    
+})
+
+const MissingCard =(props) => {
+    return (
+        <View style={[stylesGrid.boxContainer,styles.border]}>
+                <Image style={[styles.body,styles.borderTop]} source={{uri: props.fotoMissing}} />
+                <Text style={styles.missingName}>{props.missingName}</Text>
+        </View>
+        );
+}
+
+
+const HomeScreen = () => {
     const [missing, setMissing] = useState([]);
+
     useEffect(() => {
         const getMissing = async () => {
-                var data = [];
+            var data = [];
             try {
-                    firestore().collection('missing-board').orderBy("date").get()
+                firestore().collection('missing-board').orderBy("date").get()
                     .then(querySnapshot => {
                         querySnapshot.forEach(documentSnapshot => {
                             data.push(documentSnapshot.data());
                         });
                         setMissing(data);
-                });
+                    });
             }
             catch (error) {
                 console.log(error);
@@ -27,57 +120,26 @@ const Missing = () => {
     }, []);
 
     return (
-        <ScrollView style>
-            {missing.map((single) => {
-            return (
-                <View style={addItemStyles.wrapper}>
-            <View>
-                <Text>Item to give cash credit for:</Text>
-                <View style={{flexDirection:"row"}}>
-                    <View style={{flex:1}}>
-                        <TextInput placeholder="Test" style={{justifyContent: 'flex-start',}} />
-                    </View>
-                    <View style={{flex:1}}>
-                        <TextInput placeholder="Test" style={{justifyContent: 'flex-end',}} />
-                    </View>
+        <View style={stylesGrid.scrollContainer}>
+            <Text style={styles.missingBigText}>Missing Board</Text>
+            <ScrollView style={stylesGrid.scrollContainer}>
+                <View style={stylesGrid.sectionContainer}>
+                    {missing.map((post) => {
+                        return (
+                            <MissingCard
+                                key = {post.missingID}
+                                fotoMissing = {post.fotoMissing}
+                                missingName =   {post.missingName}
+                            />
+                        )
+                    })}
                 </View>
-            </View>
-
+            </ScrollView>
         </View>
-            )
-      }) }
-        </ScrollView> 
+
     );
 };
 
-const addItemStyles  = StyleSheet.create({
-    wrapper: {
-        padding: 10,
-        backgroundColor: '#FFFFFF'
-    },
-    inputLabels: {
-        fontSize: 16,
-        color: '#000000',
-        marginBottom: 7,
-    },
-    inputField: {
-        backgroundColor: '#EEEEEE',
-        padding: 10,
-        color: '#505050',
-        height: 50
-    },
-    inputWrapper: {
-        paddingBottom: 20,
-    },
-    saveBtn: {
-        backgroundColor: '#003E7D',
-        alignItems: 'center',
-        padding: 12,
-    },
-    saveBtnText: {
-        color: '#FFFFFF',
-        fontSize: 18,
-    }
-});
 
-export default Missing;
+
+export default HomeScreen;
