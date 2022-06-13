15161eccac3af1927ea618007b0d610c44e95c57
Amatias13 <80266371+Amatias13@users.noreply.github.com>
2022-06-13 23:50:09 +0100

    update touch

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, View, Image, Text, StyleSheet, Dimensions } from 'react-native';
+import { ScrollView, View, Image, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
+
+import MissingPost from '../Components/MissingPost/index.js';
 
 const rows = 3;
 const cols = 2;
@@ -11,30 +13,30 @@ const height = 200;
 
 const stylesGrid = StyleSheet.create({
     scrollContainer: {
-      flex: 1,
+        flex: 1,
     },
     sectionContainer: {
-      flex: 1,
-      flexDirection: 'row',
-      flexWrap: 'wrap',
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
     },
     boxContainer: {
-      marginTop: marginVertical,
-      marginBottom: marginVertical,
-      marginLeft: marginHorizontal,
-      marginRight: marginHorizontal,
-      width: width,
-      height: height,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#FFFFFF',
+        marginTop: marginVertical,
+        marginBottom: marginVertical,
+        marginLeft: marginHorizontal,
+        marginRight: marginHorizontal,
+        width: width,
+        height: height,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
     },
-  });
+});
 
 const styles = StyleSheet.create({
     body: {
         position: 'absolute',
-        top:0,
+        top: 0,
         height: '90%',
         width: '100%',
     },
@@ -44,15 +46,15 @@ const styles = StyleSheet.create({
         marginLeft: 'auto',
         marginVertical: 10,
         fontSize: 24,
-        color: 'black',    
-        
+        color: 'black',
+
     },
 
     missingName: {
         position: 'absolute',
         bottom: 0,
         fontWeight: '500',
-        fontSize: 17, 
+        fontSize: 17,
         color: '#333333',
         marginRight: 'auto',
         marginLeft: 'auto',
@@ -67,7 +69,7 @@ const styles = StyleSheet.create({
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 3 },
         shadowOpacity: 0.5,
-        shadowRadius: 5,  
+        shadowRadius: 5,
     },
 
     borderTop: {
@@ -76,23 +78,27 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 10,
         marginBottom: 20,
         shadowColor: 'black',
-        elevation: 5,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 3 },
         shadowOpacity: 0.5,
-        shadowRadius: 5,  
+        shadowRadius: 5,
     },
 
-    
+
 })
 
-const MissingCard =(props) => {
+const MissingCard = (props) => {
     return (
-        <View style={[stylesGrid.boxContainer,styles.border]}>
-                <Image style={[styles.body,styles.borderTop]} source={{uri: props.fotoMissing}} />
+        <TouchableOpacity onPress={() => {
+            this.props.navigation.navigate('MissingPost')
+        }}
+        >
+            <View style={[stylesGrid.boxContainer, styles.border]} >
+                <Image style={[styles.body, styles.borderTop]} source={{ uri: props.fotoMissing }} />
                 <Text style={styles.missingName}>{props.missingName}</Text>
-        </View>
-        );
+            </View>
+        </TouchableOpacity>
+    );
 }
 
 
@@ -127,9 +133,10 @@ const HomeScreen = () => {
                     {missing.map((post) => {
                         return (
                             <MissingCard
-                                key = {post.missingID}
-                                fotoMissing = {post.fotoMissing}
-                                missingName =   {post.missingName}
+                                key={post.missingID}
+                                fotoMissing={post.fotoMissing}
+                                missingName={post.missingName}
+
                             />
                         )
                     })}
