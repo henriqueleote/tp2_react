9ae4a740b48d8aab154d6d45737beac97ad724ae
Amatias13 <80266371+Amatias13@users.noreply.github.com>
2022-06-13 01:11:37 +0100

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,83 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet, Image,TextInput } from 'react-native';
+import { Component } from 'react/cjs/react.production.min';
+import firestore from '@react-native-firebase/firestore';
+import { ScrollView } from 'react-native-gesture-handler';
 
+const Missing = () => {
+    const [missing, setMissing] = useState([]);
+    useEffect(() => {
+        const getMissing = async () => {
+                var data = [];
+            try {
+                    firestore().collection('missing-board').orderBy("date").get()
+                    .then(querySnapshot => {
+                        querySnapshot.forEach(documentSnapshot => {
+                            data.push(documentSnapshot.data());
+                        });
+                        setMissing(data);
+                });
+            }
+            catch (error) {
+                console.log(error);
+            }
+        };
 
-const HomeScreen = ({ navigation }) => {
+        getMissing();
+    }, []);
+
     return (
-        <View>
-            <Text>HELLO WORLD</Text>
+        <ScrollView style>
+            {missing.map((single) => {
+            return (
+                <View style={addItemStyles.wrapper}>
+            <View>
+                <Text>Item to give cash credit for:</Text>
+                <View style={{flexDirection:"row"}}>
+                    <View style={{flex:1}}>
+                        <TextInput placeholder="Test" style={{justifyContent: 'flex-start',}} />
+                    </View>
+                    <View style={{flex:1}}>
+                        <TextInput placeholder="Test" style={{justifyContent: 'flex-end',}} />
+                    </View>
+                </View>
+            </View>
+
         </View>
+            )
+      }) }
+        </ScrollView> 
     );
 };
 
-export default HomeScreen;
+const addItemStyles  = StyleSheet.create({
+    wrapper: {
+        padding: 10,
+        backgroundColor: '#FFFFFF'
+    },
+    inputLabels: {
+        fontSize: 16,
+        color: '#000000',
+        marginBottom: 7,
+    },
+    inputField: {
+        backgroundColor: '#EEEEEE',
+        padding: 10,
+        color: '#505050',
+        height: 50
+    },
+    inputWrapper: {
+        paddingBottom: 20,
+    },
+    saveBtn: {
+        backgroundColor: '#003E7D',
+        alignItems: 'center',
+        padding: 12,
+    },
+    saveBtnText: {
+        color: '#FFFFFF',
+        fontSize: 18,
+    }
+});
+
+export default Missing;
