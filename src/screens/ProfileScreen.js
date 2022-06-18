bbadf19f43d0805599c437e8e205fdf91f7b2e3d
UlyanaPyrohovska <ullasochka2015@gmail.com>
2022-06-18 17:45:02 +0100

    Profile page in progress

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import {
   View,
   Text,
   Image,
-    TouchableOpacity,
+  TouchableOpacity,
   Button,
   StyleSheet,
   ScrollView,
@@ -15,41 +15,59 @@ import firestore from '@react-native-firebase/firestore';
 import LoginScreen from './Login';
 
 async function logout() {
-    await auth().signOut()
+  await auth().signOut()
     .then(() => {
-        console.log('logged out');
-        return <LoginScreen />;
-        })
-        .catch(error => console.log(error));
+      console.log('logged out');
+      return <LoginScreen />;
+    })
+    .catch(error => console.log(error));
 }
 
 const ProfileScreen = () => {
-    const [user, setUser] = useState([]);
-    useEffect(() => {
-        const getUser = async () => {
-            try {
-                firestore().collection('users').doc(auth().currentUser.uid).onSnapshot(documentSnapshot => {
-                        setUser(documentSnapshot.data());
-                    })
-            }
-            catch (error) {
-                console.log(error);
-            }
-        };
-        getUser();
-    }, []);
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        firestore().collection('users').doc(auth().currentUser.uid).onSnapshot(documentSnapshot => {
+          setUser(documentSnapshot.data());
+        })
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
+    getUser();
+  }, []);
 
-    return (
-        <ScrollView style={{padding:20}}>
-          <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
-            <ScrollView style={styles.container} contentContainerStyle={{justifyContent: 'center', alignItems: 'center'}} showsVerticalScrollIndicator={false}>
-              <Image style={styles.userImg} source={{uri: user.imageURL }} />
-              <Text style={styles.userName}> {user.firstName} {user.lastName} </Text>      
-              <Button title='Logout' onPress={() => logout()}></Button>
-            </ScrollView>
-          </SafeAreaView>
-        </ScrollView> 
-    );
+  return (
+    <ScrollView>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+        <ScrollView style={styles.container} contentContainerStyle={{ justifyContent: 'center', alignItems: 'center' }} showsVerticalScrollIndicator={false}>
+          <Image style={styles.userImg} source={user.imageURL !== 'null' ? { uri: user.imageURL } : require('../Images/unknown.png')} />
+          <View style={styles.editName}>
+            <Text style={styles.userName}> {user.firstName} {user.lastName} </Text>
+            <TouchableOpacity >
+              <Image source={require('../Images/pencil.png')} style={{height: 21, width: 21, marginLeft: 10}}></Image>
+            </TouchableOpacity>
+          </View>
+          <Text>{user.userType}</Text>
+          <View style={styles.info}>
+            <Text >Email: {user.email}</Text>
+            <Text >Contact: {user.phoneNumber}</Text>
+          </View>
+          <TouchableOpacity style={styles.buttons}>
+            <Text style={{ color: '#ffffff' }}>Publications in Community</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttons}>
+            <Text style={{ color: '#ffffff' }}>Publications in Missing</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.logout} onPress={() => logout()} >
+            <Text style={{ color: '#ffffff' }}>LOG OUT</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </SafeAreaView>
+    </ScrollView>
+  );
 };
 
 
@@ -67,7 +85,7 @@ const styles = StyleSheet.create({
     borderRadius: 75,
   },
   userName: {
-    fontSize: 18,
+    fontSize: 24,
     fontWeight: 'bold',
     marginTop: 10,
     marginBottom: 10,
@@ -115,5 +133,43 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: '#666',
     textAlign: 'center',
+  },
+  buttons: {
+    width: 190,
+    marginTop: 30,
+    borderRadius: 9,
+    padding: 10,
+    borderTopLeftRadius: 6,
+    borderBottomRightRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#DE2F2F',
+    elevation: 10,
+    marginRight: 150
+  },
+  info: {
+    marginTop: 40,
+    fontSize: 14,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: 70,
+    marginRight: 150
+  },
+  editName: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  logout: {
+    width: 100,
+    marginTop: 30,
+    borderRadius: 9,
+    padding: 10,
+    borderTopLeftRadius: 6,
+    borderBottomRightRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#2E98F0',
+    elevation: 10,
   },
 });
